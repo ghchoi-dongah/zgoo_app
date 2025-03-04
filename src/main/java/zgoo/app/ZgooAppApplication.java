c88e058fa56bf26f8de3e9342472599fe323c522
ghchoi-dongah <ghchoi@dongahelecomm.co.kr>
2025-03-04 12:14:10 +0900

    first commit

---
+++ src/main/java/zgoo/app/ZgooAppApplication.java
@@ -0,0 +1,13 @@
+package zgoo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZgooAppApplication.class, args);
+	}
+
+}
