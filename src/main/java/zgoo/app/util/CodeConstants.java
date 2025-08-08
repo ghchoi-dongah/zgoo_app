2328808be56acb04817ed985f38090696d58da87
ghchoi-dongah <ghchoi@dongahelecomm.co.kr>
2025-08-08 10:47:19 +0900

    공통코드 상수 재설정

---
+++ src/main/java/zgoo/app/util/CodeConstants.java
@@ -0,0 +1,23 @@
+package zgoo.app.util;
+
+public class CodeConstants {
+    // grp code
+    public static final String NOTICETYPECD   = "NOTICETYPECD";   // 공지유형
+    public static final String FAQKIND        = "FAQKIND";        // FAQ 구분코드
+    public static final String VOCTYPE        = "VOCTYPE";        // 문의유형
+
+    // common code
+    // 회원상태코드
+    public static final String MSTNORMAL      = "MSTNORMAL";    // 정상
+    public static final String MSTATOUT       = "MSTATOUT";     // 탈퇴
+    public static final String MSTDORMANT     = "MSTDORMANT";   // 휴면
+    // 회원결제카드등록상태
+    public static final String MCNOTREG       = "MCNOTREG";     // 미등록
+    public static final String MCNOBILLKEY    = "MCNOBILLKEY";  // 빌키없음
+    public static final String MCNORMAL       = "MCNORMAL";     // 정상
+    // 문의경로
+    public static final String VOCAPP         = "VOCAPP";       // 모바일앱
+    // 조치상태
+    public static final String STANDBY        = "STANDBY";      // 대기
+    public static final String COMPLETE       = "COMPLETE";     // 완료
+}
--- src/main/java/zgoo/app/util/GrpcodeConstants.java
+++
@@ -1,6 +0,0 @@
-package zgoo.app.util;
-
-public class GrpcodeConstants {
-    public static final String NOTICETYPECD = "NOTICETYPECD";   // 공지유형
-    public static final String FAQKIND      = "FAQKIND";        // FAQ 구분코드
-}
